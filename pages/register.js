3f9f7f711bb421ff0a7cfe0f2dc09a8e279b6ad3
daaaanav <dj631125@gmail.com>
2023-12-13 17:02:02 +0530

    Complet Login & singup Loader

--- pages/register.js
+++ pages/register.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import { FcGoogle } from "react-icons/fc";
 import { auth } from "../firebase/firebase";
 import {
@@ -7,6 +7,10 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
 } from "firebase/auth";
+import { useAuth } from "@/firebase/auth";
+import { useRouter } from "next/router";
+import Load from "@/componets/Load";
+import Link from "next/link";
 
 const provider = new GoogleAuthProvider();
 // const provider2 = new GithubAuthProvider();
@@ -14,6 +18,14 @@ const RegisterForm = () => {
   const [username, setUsername] = useState(null);
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
+  const { authUser, isLoading , setAuthUser} = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && authUser) {
+      router.push("/");
+    }
+  }, [authUser, isLoading]);
 
   const singup = async () => {
     if (!email || !username || !password) return;
@@ -22,6 +34,12 @@ const RegisterForm = () => {
       await updateProfile(auth.currentUser, {
         displayName: username,
       });
+      setAuthUser({
+        uid: user.uid,
+        email: user.email,
+        username,
+
+      })
       console.log(user);
     } catch (error) {
       console.error("Error Is thrown", error);
@@ -36,16 +54,16 @@ const RegisterForm = () => {
     }
   };
 
-  return (
+  return isLoading || (!isLoading && authUser) ? <Load/>: (
     <main className="flex lg:h-[100vh]">
       <div className="w-full lg:w-[60%] p-8 md:p-14 flex items-center justify-center lg:justify-start">
         <div className="p-8 w-[600px]">
           <h1 className="text-6xl font-semibold">Sign Up</h1>
           <p className="mt-6 ml-1">
             Already have an account ?{" "}
-            <span className="underline hover:text-blue-400 cursor-pointer">
+            <Link href={"/login"} className="underline hover:text-blue-400 cursor-pointer">
               Login
-            </span>
+            </Link>
           </p>
 
           <div className="bg-black/[0.05] text-white w-full py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90 flex justify-center items-center gap-4 cursor-pointer group">
